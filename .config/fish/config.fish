60bf19cebeb8dde15e674faff47e69f8e8c05f92
Simon Oehrl <simon.oehrl@rwth-aachen.de>
2023-05-04 15:59:33 +0200

    Setup cahes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 if which nvim >/dev/null
   set -x EDITOR nvim
 end
@@ -8,3 +10,6 @@ and not set -q TMUX
 end
 
 source $HOME/.config/fish/custom_config.fish
+set -gx CPM_SOURCE_CACHE $HOME/.cpm
+set -gx PATH $PATH:$HOME/.local/bin
+set -gx PATH $PATH:$HOME/.cargo/bin

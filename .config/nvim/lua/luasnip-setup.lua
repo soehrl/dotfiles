0275c416d8c115ab0e560449ab16555a41ca5528
Simon Oehrl <simon.oehrl@rwth-aachen.de>
2023-05-04 15:59:03 +0200

    Update snippets

--- .config/nvim/lua/luasnip-setup.lua
+++ .config/nvim/lua/luasnip-setup.lua
@@ -1,12 +1,10 @@
 local ls = require 'luasnip'
 
 ls.add_snippets('typescriptreact', { ls.parser.parse_snippet('reactcomponent', [[
-import React from 'react';
-
 export interface $1Props {
 }
 
-export default function $1(props: React.PropsWithoutRef<$1Props>) {
+export default function $1(props: $1Props) {
   return (
     <span>$1</span>
   );
